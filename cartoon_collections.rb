db44dbd426ab857eeca0839ee8ae3410f3728b1e
Thomas Castagna <castagna.tm@gmail.com>
2019-08-19 22:11:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(dwarves)
-  dwarves.each_with_index { |index, item| puts "#{item}" }
+  dwarves.each_with_index { |index, item| puts "#{index}" }
 end
 
 def summon_captain_planet(array)
